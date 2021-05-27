2b746658ba167c6de53402f49b4ebec52acb8abb
Crist Ye <iamCristYe@outlook.com>
2021-05-27 20:04:29 +0800

    Update rename_with_unix_time.py

--- Python/rename_with_unix_time.py
+++ Python/rename_with_unix_time.py
@@ -4,10 +4,7 @@
 Rename files with the Unix time.
 The files are in Wechat folder of Android OS:
     /Tencent/MicroMsg/WeiXin/
-rules:
-    1482227010527.mp4 -> 2016-12-20 17.43.30.mp4
-    mmexport1482227010527.jpg -> 2016-12-20 17.43.30.jpg
-    wx_camera_1482227010527.mp4 -> 2016-12-20 17.43.30.mp4
+
 
 author: Gymgle
 created at: 2016-12-20
@@ -21,6 +18,7 @@ if __name__ == "__main__":
     PATH = "."
     LENMP4 = len("1482227010527.mp4")
     LENJPG = len("mmexport1482227010527.jpg")
+    LENJPEG = len("mmexport1482227010527.jpeg") #for webp as well
     LENWXCAM = len("wx_camera_1482227010527.mp4")
 
     for filename in os.listdir(PATH):
@@ -28,14 +26,16 @@ if __name__ == "__main__":
             timestamp = filename.split(".")[0]
         elif len(filename) == LENJPG and filename.startswith("mmexport"):
             timestamp = filename[len("mmexport"):-4]
+        elif len(filename) == LENJPEG and filename.startswith("mmexport"):
+            timestamp = filename[len("mmexport"):-5]
         elif len(filename) == LENWXCAM and filename.startswith("wx_camera_"):
             timestamp = filename[len("wx_camera_"):-4] 
         else:
             continue
 
-        ext = filename.split(".")[-1]
-        newName = datetime.datetime.fromtimestamp(int(timestamp)/1e3).strftime("%Y-%m-%d %H.%M.%S")
-        newName = newName + "." + ext
+       # ext = filename.split(".")[-1]
+        newName = datetime.datetime.fromtimestamp(int(timestamp)/1e3).strftime("%Y-%m-%d_%H-%M-%S")
+        newName = newName + "_" + filename # + "." + ext
 
         try:
             os.rename(filename, newName)
